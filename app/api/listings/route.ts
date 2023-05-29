434d02b66d94dbee7f88b4c65b632c3dc9976cf8
Alien-oyi <daijinmin78@gmail.com>
2023-05-28 22:01:38 -0400

    again6

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -1,44 +1,34 @@
 import { NextResponse } from "next/server";
-import prisma from "@/app/libs/prismadb";
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
 
-export async function POST(request: Request) {
-    const currentUser = await getCurrentUser(request);
-    if (!currentUser) {
-        return NextResponse.error();
-    }
-
-    const body = await request.json();
-    const { 
-        title,
-        description,
-        imageSrc,
-        category,
-        roomCount,
-        bathroomCount,
-        guestCount,
-        location,
-        price
-    } = body;
-    // Object.keys(body).forEach((value:any)=> {
-    //     if (!body[value]) {
-    //         return NextResponse.error();
-    //     }
-    // })
-    const listing = await prisma.listing.create({
-        data: {
-            title,
-            description,
-            imageSrc,
-            category,
-            roomCount,
-            bathroomCount,
-            guestCount,
-            locationValue: location.value,
-            price: parseInt(price,10),
-            userId: currentUser.id
-        }
-    })     
-    return NextResponse.json(listing);
+interface IParams {
+  listingId?: string;
 }
 
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const listing = await prisma.listing.deleteMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
