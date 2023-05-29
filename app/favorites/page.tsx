bd473e56a580efeff92252b62146b8ad461a0ce0
Alien-oyi <daijinmin78@gmail.com>
2023-05-28 20:48:22 -0400

    try again

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,28 +1,35 @@
-import EmptyState from "../components/EmptyState";
-import ClientOnly from "../components/ClientOnly";
 
-import getCurrentUser from "../actions/getCurrentUser";
-import getFavoriteListing from "../actions/getFavoriteListing";
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
+
 import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
-    const listings= await getFavoriteListing();
+    const listings = await getFavoriteListings();
     const currentUser = await getCurrentUser();
+
     if (listings.length === 0) {
         return (
             <ClientOnly>
-                <EmptyState 
-                title="No favorites yet"
-                subtitle="You haven't favorited any listings yet"/>
+                <EmptyState
+                    title="No favorites found"
+                    subtitle="Looks like you have no favorite listings."
+                />
             </ClientOnly>
-        )
+        );
     }
+
     return (
         <ClientOnly>
-            <FavoritesClient 
-            listings={listings}
-            currentUser={currentUser}/>
+            <FavoritesClient
+                listings={listings}
+                currentUser={currentUser}
+            />
         </ClientOnly>
-
-    )
+    );
 }
+
+export default ListingPage;
